284af792ce141abfb3d1a69aef4baa5294d66d4c
Edehify <edehifeanyi16@yahoo.com>
2025-06-19 18:27:50 +0100

    feat: Select default color from color picker

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function createGrid(size) {
     div.classList.add("square");
     div.style.width = `${squareSize - 2}px`;  // subtract for borders
     div.style.height = `${squareSize - 2}px`;
-    div.style.backgroundColor = "blue"; // for testing
+    div.style.backgroundColor = `${selectedColor}`; // for testing
     main.appendChild(div);
   }
 }
@@ -26,15 +26,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // 🔄 Run when input value changes
 resizeBtn.addEventListener("click", () => {
+  // create user prompt by clicking
    let userInput = prompt("Enter a grid size from 1 to 100:");
+   // convert the value to interger
   const value = parseInt(userInput);
+  // validate input
   if (!isNaN(value) && value >= 1 && value <= 100) {
     createGrid(value);
+    // update ui value display
     resizeBtn.textContent = `${value} × ${value}`; 
   } else {
     console.warn("Enter a number between 1 and 100.");
   }
 });
+// get color picker
+const inputColor = document.querySelector("#color-picker");
+let selectedColor = inputColor.value;
+
+
 
 
 
